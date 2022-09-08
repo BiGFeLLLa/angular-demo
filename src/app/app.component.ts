e801997691c888022bd8899049f7d3efb67c212b
Akash <amishra280302@gmail.com>
2022-09-08 16:33:15 +0530

    apppppp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  count = 1;
+
+  listOfNumbers = [1, 32, 32, 55];
+  listOfCharacters = ['Delhi', 'Mumbai', 'Bangalore', 'Pune'];
+
+  decreaseCount = () => {
+    console.log('Button clicked')
+    this.count--;
+  }
+  increaseCount = () => {
+    console.log('Button clicked')
+    this.count++;
+  }
+  arr= [0,1,2,3,4,5,6,7,8,9];
+  A= 0;
+  B= 0;
+  C= 0;
+  
+  add = () => {
+    this.C = this.A + this.B;
+  }
+  subs = () => {
+    this.C = this.A - this.B;
+  }
+  mult = () => {
+    this.C = this.A * this.B;
+  }
+  divi = () => {
+    this.C = this.A / this.B;
+  }
 }
